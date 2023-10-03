ce2bfd68e900446a0c50abd1154861c7242fef13
KnowitRobertFred <robert.fred@knowit.se>
2023-10-03 20:20:55 +0200

    Tagit bort sidor.

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,11 +24,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://www.knowit.se',
+    baseURL: 'https://www.saucedemo.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    
+    testIdAttribute: 'data-test',
   },
 
   /* Configure projects for major browsers */
--- tests/knowit-home-page.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import { StartPage } from '../Page-Objects/StartPage';
-
-test('Home page should open', async ({page}) =>{
-  let startPage = new StartPage(page);
-  await startPage.gotoPage();
-  await startPage.acceptAllCookies();
-  await startPage.openMenu();
-});
