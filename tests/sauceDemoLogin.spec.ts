ab78b1971e166f4ff92fefa8dd2bdd8b64d40297
KnowitRobertFred <robert.fred@knowit.se>
2023-10-05 14:51:28 +0200

    Tagit bort page-object etc.

---
+++ tests/sauceDemoLogin.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@playwright/test';
+import { Console } from 'console';
+
+test('Login to sauce demo', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  const usernameInput = page.getByPlaceholder('Username');
+  await expect(usernameInput).toBeVisible();
+
+  await usernameInput.fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Products')).toBeVisible();
+});
